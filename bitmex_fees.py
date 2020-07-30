16cc21be9c9813b69fb2417ee78afffe08b2bf93
drewsmith9 <amsmith1010@yahoo.com>
2020-07-30 17:30:27 -0500

    added bitmex_fees pythton file and object

---
+++ bitmex_fees.py
@@ -0,0 +1,60 @@
+
+bitmex_fees = {
+'Bitcoin': {
+    'Ticker': 'XBT',
+    'Leverage': '100x',
+    'Maker Fee': '-0.0250%',
+    'Taker Fee': '0.0750%',
+    'Settlement Fee': '0.0500%' 
+},
+'Bitcoin Cash': {
+    'Ticker': 'BCH',
+    'Leverage': '20x',
+    'Maker Fee': '-0.0500%',
+    'Taker Fee': '0.2500%',
+    'Settlement Fee': '0.0000%' 
+},
+'Cardano': {
+    'Ticker': 'ADA',
+    'Leverage': '20x',
+    'Maker Fee': '-0.0500%',
+    'Taker Fee': '0.2500%',
+    'Settlement Fee': '0.0000%' 
+},
+'EOS Token': {
+    'Ticker': 'EOS',
+    'Leverage': '20x',
+    'Maker Fee': '-0.0500%',
+    'Taker Fee': '0.2500%',
+    'Settlement Fee': '0.0000%' 
+},
+'Ethereum': {
+    'Ticker': 'ETH',
+    'Leverage': '50x',
+    'Maker Fee': '-0.0500%',
+    'Taker Fee': '0.2500%',
+    'Settlement Fee': '0.0000%' 
+},
+'Litecoin': {
+    'Ticker': 'LTC',
+    'Leverage': '33.33x',
+    'Maker Fee': '-0.0500%',
+    'Taker Fee': '0.2500%',
+    'Settlement Fee': '0.0000%' 
+},
+'Ripple': {
+    'Ticker': 'XRP',
+    'Leverage': '20x',
+    'Maker Fee': '-0.0500%',
+    'Taker Fee': '0.2500%',
+    'Settlement Fee': '0.0000%' 
+},
+'Tron': {
+    'Ticker': 'TRX',
+    'Leverage': '20x',
+    'Maker Fee': '-0.0500%',
+    'Taker Fee': '0.2500%',
+    'Settlement Fee': '0.0000%'
+}}
+
+# print(bitmex_fees['Bitcoin']['Ticker'],bitmex_fees['Bitcoin']['Leverage'],bitmex_fees['Bitcoin']['Taker Fee'])
