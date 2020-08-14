bbf456787a001e780f6faa794d4496feee9e3a77
drewsmith9 <amsmith1010@yahoo.com>
2020-08-14 09:41:12 -0500

    no changes

--- getGraphPrice.py
+++ getGraphPrice.py
@@ -5,7 +5,10 @@ import plotly.graph_objects as go
 
 while True:
     data = cg.get_price('bitgear', vs_currencies='usd')
-    stamp = str(datetime.datetime.now())[:22]
-    price = '$'+str(568.441*float(data['bitgear']['usd']))[:6]
-    print([stamp, price])
-    time.sleep(5)
+    curr_price = cg.get_coin_market_chart_by_id('bitgear', vs_currency='usd', days='max')
+    stamp = str(datetime.datetime.fromtimestamp(int(curr_price['prices'][0][0])/1000))
+    price = str(curr_price['prices'][0][1])[:6]
+    wallet = '$'+str(568.441*float(curr_price['prices'][0][1]))[:6]
+
+    print([stamp, price, wallet])
+    time.sleep(60)
